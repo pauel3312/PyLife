eafa0dddb9a919d7a810db57f8c2cd159690706b
pauel3312 <pau.lacazedieu@gmail.com>
2023-03-06 13:25:24 +0100

    OMW to selector logic and table edit capability

--- GameOfLifeLogic.py
+++ GameOfLifeLogic.py
@@ -1,3 +1,6 @@
+commands_calls = {'quit': False, 'up': False,  'left': False,  'down': False,  'right': False,  'change': False, }
+
+
 def empty_table(width, length):
     return [[False] * width] * length
 
@@ -36,6 +39,59 @@ def life_step(table, generation, rule=((3,), (2, 3))):
             else:
                 new_table[x].append(value)
     return new_table, generation + 1, get_population(new_table)
+
+
+class SelectorTable:
+    def __init__(self, width, length):
+        self._table = empty_table(width, length)
+        self.position = [0, 0]
+
+    def move_up(self):
+        self.position[1] -= 1
+
+    def move_left(self):
+        self.position[0] -= 1
+
+    def move_down(self):
+        self.position[1] += 1
+
+    def move_right(self):
+        self.position[0] += 1
+
+    def change_state(self):
+        self._table[self.position[0]][self.position[1]] = not self._table[self.position[0]][self.position[1]]
+
+    # function to get value of _age
+    def get_table(self):
+        return self._table
+
+    # function to set value of _age
+    def set_table(self, a):
+        print("WARNING: Table not default")
+        self._table = a
+
+    # function to delete _age attribute
+    def del_table(self):
+        del self._table
+        del self
+
+    table = property(get_table, set_table, del_table)
+
+    def edit_loop(self, draw_callback=None):
+        global commands_calls
+        while not commands_calls['quit']:
+            if commands_calls['up']:
+                self.move_up()
+            if commands_calls['left']:
+                self.move_left()
+            if commands_calls['down']:
+                self.move_down()
+            if commands_calls['right']:
+                self.move_right()
+            if commands_calls['change']:
+                self.change_state()
+            if draw_callback is not None:
+                draw_callback(self.table)
 
 
 if __name__ == '__main__':
