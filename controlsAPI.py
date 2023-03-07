a6853c24cd1d46c1ee806a313fa1e9903fa0dc3b
pauel3312 <pau.lacazedieu@gmail.com>
2023-03-07 13:24:08 +0100

    Making the global set of pressed keys. The API os not yet working.

--- controlsAPI.py
+++ controlsAPI.py
@@ -1,20 +1,23 @@
 import keyboard
-from functools import partial
 
-was_pressed = False
+pressed_keys_set = set()
 
 
-# TODO: Make a setup method for the API: one per mode that are called to set the `on_press_key`s and `on_release_key`s
+# TODO: transform method for the set to meet the API command spec
 def keys_to_API(mode):
-    global was_pressed
     print(mode)
-    was_pressed = True
-    # TODO: return the global key presses set, converted to comply with the API spec.
-
-# TODO: Callback method for the key presses, so they come to a global set object
 
 
-keyboard.on_press_key('ctrl', keys_to_API)
+def keypress_callback(key):
+    string_key, string_event = str(key).split('(')[1].strip(')').split(' ')
+    if string_event == 'up' and string_key in pressed_keys_set:
+        pressed_keys_set.discard(string_key)
+    elif string_event == 'down' and string_key not in pressed_keys_set:
+        pressed_keys_set.add(string_key)
 
-while not was_pressed:
-    None
+
+keyboard.hook(keypress_callback)
+
+if __name__ == '__main__':
+    while True:
+        print(pressed_keys_set)
