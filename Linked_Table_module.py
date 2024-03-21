f0503286262b99ffdaf0e3eb786be3a2b1363433
pauel3312 <pau.lacazedieu@gmail.com>
2024-03-21 12:57:04 +0100

    refactored expand_along to allow for the new expansion system that expands the grid by 1 instead of by 2, and that will be overall simpler to implement.

--- Linked_Table_module.py
+++ Linked_Table_module.py
@@ -136,58 +136,73 @@ def expand_corner(corner: LinkedTableNode) -> list[LinkedTableNode]:
     return [new_corner, new_final_edge_0, new_final_edge_1]
 
 
-def expand_along(edge: LinkedTableNode) -> set[LinkedTableNode]:
+def expand_along(edge: LinkedTableNode) -> Optional[set[LinkedTableNode]]:
     node_under_current: Optional[LinkedTableNode] = None
     node_under_next: Optional[LinkedTableNode] = None
-    for node in edge.hooked:
-        if len(node) == 7:
-            if node_under_current is not None:
-                raise ValueError("base structure invalid: too many 7-neighbour adjacencies")
-            node_under_current = node
-        if len(node) == 6:
-            if node_under_next is not None:
-                raise ValueError("base structure invalid: too many 6-neighbour adjacencies")
-            node_under_next = node
-        if node_under_next is not None and node_under_current is not None:
-            break
-
-    node_after_next: Optional[LinkedTableNode] = None
-    for node in node_under_next.hooked:
-        if len(node) == 5:
-            node_after_next = node
-            break
-    if node_after_next is None:
-        for node in node_under_next.hooked:
+    if 4 not in {len(x) for x in edge.hooked}:
+        for node in edge.hooked:
+            if len(node) == 7:
+                if node_under_current is not None:
+                    raise ValueError("base structure invalid: too many 7-neighbour adjacencies")
+                node_under_current = node
             if len(node) == 6:
+                if node_under_next is not None:
+                    raise ValueError("base structure invalid: too many 6-neighbour adjacencies")
+                node_under_next = node
+            if node_under_next is not None and node_under_current is not None:
+                break
+
+        node_after_next: Optional[LinkedTableNode] = None
+        for node in node_under_next.hooked:
+            if len(node) == 5:
                 node_after_next = node
                 break
-        else:
-            raise ValueError("Invalid links in base table.")
+        if node_after_next is None:
+            for node in node_under_next.hooked:
+                if len(node) == 6:
+                    node_after_next = node
+                    break
+            else:
+                raise ValueError("Invalid links in base table.")
 
-        node_under_other_end: Optional[LinkedTableNode] = None
-        node_other_end: Optional[LinkedTableNode] = None
-        for node in node_after_next.hooked:
+            node_under_other_end: Optional[LinkedTableNode] = None
+            node_other_end: Optional[LinkedTableNode] = None
+            for node in node_after_next.hooked:
+                if len(node) == 7:
+                    node_under_other_end = node
+                if len(node) == 4:
+                    node_other_end = node
+                if node_under_other_end is not None and node_other_end is not None:
+                    break
+            else:
+                raise ValueError("Invalid links in base table")
+
+            adjacent_node = LinkedTableNode(hooked={node_under_current,
+                                                    node_under_next,
+                                                    node_after_next,
+                                                    edge})
+            closing_node = LinkedTableNode(hooked={node_under_next,
+                                                   node_after_next,
+                                                   node_under_other_end,
+                                                   node_other_end,
+                                                   adjacent_node})
+            return {adjacent_node, closing_node}
+        else:
+            return {LinkedTableNode(hooked={edge, node_under_current, node_under_next, node_after_next})}
+    else:  # the current edge has a 4-adjacency neighbour, meaning it is at the end of the line.
+        for node in edge.hooked:
             if len(node) == 7:
-                node_under_other_end = node
+                if node_under_current is not None:
+                    raise ValueError("base structure invalid: too many 7-neighbour adjacencies")
+                node_under_current = node
             if len(node) == 4:
-                node_other_end = node
-            if node_under_other_end is not None and node_other_end is not None:
+                if node_under_next is not None:
+                    raise ValueError("base structure invalid: too many 6-neighbour adjacencies")
+                node_under_next = node
+            if node_under_next is not None and node_under_current is not None:
                 break
-        else:
-            raise ValueError("Invalid links in base table")
-
-        adjacent_node = LinkedTableNode(hooked={node_under_current,
-                                                node_under_next,
-                                                node_after_next,
-                                                edge})
-        closing_node = LinkedTableNode(hooked={node_under_next,
-                                               node_after_next,
-                                               node_under_other_end,
-                                               node_other_end,
-                                               adjacent_node})
-        return {adjacent_node, closing_node}
-    else:
-        return {LinkedTableNode(hooked={edge, node_under_current, node_under_next, node_after_next})}
+        LinkedTableNode(hooked={edge, node_under_current, node_under_next})
+        return None
 
 
 def expand(base: set[LinkedTableNode]) -> set[LinkedTableNode]:
